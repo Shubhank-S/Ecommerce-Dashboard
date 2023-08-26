67909ab6d027caa92806f14d6aef70eaadc8695b
Shubhank Sharma <shubhanksharma345@gmail.com>
2023-08-26 17:20:42 +0530

    Pages Added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Logout from "./pages/Logout";
 
 function App() {
   return (
     <main>
       <BrowserRouter>
         <Routes>
-          <Route path="/" />
-          <Route path="/about" />
+          <Route path="/" element={<Home />} />
+          <Route path="/logout" element={<Logout />} />
         </Routes>
       </BrowserRouter>
     </main>
